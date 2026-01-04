9b918deaccda8c084b8b05a5ac7a96625f1f7dc2
meke16 <habtamucherinet40@gmail.com>
2026-01-04 21:34:47 +0300

    configure db to match local and work again on repo

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php 
 
-$conn = new mysqli("localhost" , "root" , "" ,"hu" );
+$conn = new mysqli("localhost" , "root" , "Adey@@1997" ,"hu" );
 
 if(!$conn) {
     echo die("". mysqli_error($conn));
